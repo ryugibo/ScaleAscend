e05a1816397b0b6cd6d6b384b977dcc5ab438ff7
ryugibo <7928632+ryugibo@users.noreply.github.com>
2025-05-24 15:33:16 +0900

    AB#16 새 C++ 프로젝트 생성

---
+++ Source/ScaleAscend/ScaleAscend.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ScaleAscend.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ScaleAscend, "ScaleAscend" );
